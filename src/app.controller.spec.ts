ac443b193e6d3eab6578b3804459d81a0735e22f
koala819 <x.genolhac@gmail.com>
2025-05-28 10:32:39 +0200

    fix: test run with success

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { getConnectionToken } from '@nestjs/mongoose'
 
 describe('AppController', () => {
   let appController: AppController
@@ -8,7 +9,16 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: getConnectionToken(),
+          useValue: {
+            once: jest.fn(),
+            name: 'test-db',
+          },
+        },
+      ],
     }).compile()
 
     appController = app.get<AppController>(AppController)
